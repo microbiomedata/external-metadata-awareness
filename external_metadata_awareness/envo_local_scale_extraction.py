ec907d0f6617d32eec337cd295b3695854851db7
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2024-08-30 16:13:46 -0700

    add in ability to exclude single terms, as well as include term post-extraction

--- external_metadata_awareness/envo_local_scale_extraction.py
+++ external_metadata_awareness/envo_local_scale_extraction.py
@@ -66,20 +66,22 @@ def exclude_terms(full_list, exclusion_list):
     return [item for item in full_list if item not in exclusion_list]
 
 
-def create_exclude_solo_terms(exclusion_terms: List[str], adapter) -> List[str]:
+def retrieve_individual_terms(terms_to_retrieve: List[str], adapter) -> List[str]:
     """
-    Creates a list of CURIEs to exclude based on the provided list of terms.
+    Creates a list of CURIEs based on the provided list of term labels.
 
-    :param exclusion_terms: List of term labels to exclude.
+    :param terms_to_retrieve: List of term labels.
     :param envo: The ontology adapter.
     """
-    all_ids_to_exclude = []
+    all_ids = []
 
-    for term_label in exclusion_terms:
+    for term_label in terms_to_retrieve:
         # Find the CURIE for the label
         list_to_exclude = onto_query([term_label], adapter)
-        all_ids_to_exclude.extend(list_to_exclude)
-    return list(set(all_ids_to_exclude))
+        print("term_label", term_label)
+        print("list_to_exclude", list_to_exclude)
+        all_ids.extend(list_to_exclude)
+    return list(set(all_ids))
 
 
 def extract_terms_to_file(oak_config_file, extraction_config):
@@ -96,17 +98,25 @@ def extract_terms_to_file(oak_config_file, extraction_config):
 
     exclusion_terms_from_text = create_text_exclusion_list(extraction_config.get('text_exclusions', []),
                                                            envo)
-    excluded_terms = create_exclude_solo_terms(extraction_config.get('term_exclusions', []), envo)
 
-    exclusion_list = exclusion_terms_and_children + exclusion_terms_from_text + excluded_terms
+    exclude_single_terms = retrieve_individual_terms(extraction_config.get('exclude_single_terms', []), envo)
+    solo_inclusion_terms = extraction_config.get('post_process_inclusion_single_terms', [])
+    logging.info("solo_inclusion_terms", solo_inclusion_terms)
+    post_process_inclusion_single_terms = retrieve_individual_terms(extraction_config.get('post_process_inclusion_single_terms', []), envo)
+    logging.info("post_process_inclusion_terms", post_process_inclusion_single_terms)
+
+
+    exclusion_list = exclusion_terms_and_children + exclusion_terms_from_text + exclude_single_terms
     logging.info(f"Length of excluded terms and descendants: {len(exclusion_terms_and_children)}")
     logging.info(f"Length of excluded terms from text: {len(exclusion_terms_from_text)}")
-    logging.info(f"Length of excluded terms from solo terms: {len(excluded_terms)}")
+    logging.info(f"Length of excluded terms from solo terms: {len(post_process_inclusion_single_terms)}")
 
     remaining_items = exclude_terms(initial_term_list, exclusion_list)
     logging.info(f"Length of remaining items: {len(remaining_items)}")
 
-    results = onto_query(remaining_items, envo, labels=True)
+    final_list_to_retrieve = post_process_inclusion_single_terms + remaining_items
+
+    results = onto_query(final_list_to_retrieve, envo, labels=True)
 
     # Write the results to the output file specified in the extraction config
     output_file_path = extraction_config['output']
